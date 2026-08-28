6d6eb31cb87a0fa9f8a8f37dcd4e2988c6bd80e8
Gustavo Ewerthon Alves Silva <gustavoewerthon@gmail.com>
2026-08-28 15:17:26 -0300

    configurando o menu

--- cabecalho.py
+++ cabecalho.py
@@ -1,4 +1,20 @@
-import streamlit
+import streamlit as str
 
 class Cabecalho:
-    self.image
+    def __init__(self):
+        self.image="icones/logo.png"
+        self.menu = ["Home","Sobre", "Contato"]
+        self.menu_selecionado = "Home"
+        self.titulo = "solutech"
+
+    def configurar_cabecalho(self):
+        str.set_page_config(
+            page_title=self.titulo,
+            page_icon=self.image,
+            layout="wide",
+            initial_sidebar_state="expanded"
+        )
+        col1, col2 = str.columns([1, 3])   
+        col1.image(self.image, width=100)
+        col2.title(self.titulo)
+        col3.menu = str.selectbox("Menu", self.menu, index=self.menu.index(self.menu_selecionado))
